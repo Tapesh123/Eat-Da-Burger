540fa7a77ab53cacd017bccf558c1d48a2f73e0f
Patkul <tshah011@gmail.com>
2018-05-23 19:53:56 -0400

    just starting

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured) 
+  VALUES
+("VeggieBurger", true),
+("Chicken Maharaj", false),
+("Chicken Burger", true),
+('Wendys Hamburger', true),
+('Tequila Lime Burger',false),
+('Black Bean Burger', false);  
